ef20a4596f7b4edcecefa958a2e7858ed60cad16
Sebastian Villarreal <Sebastian@Sebastians-MacBook-Air.local>
2016-02-09 14:59:23 -0600

    questions method

---
+++ basics_difficult.rb
@@ -0,0 +1,10 @@
+def questions(number, question)
+
+  puts "This is question number #{number.to_s}."
+  puts "The question is '#{question}'."
+
+end
+
+questions(1, "where's the money, lebowski?")
+
+questions(2, "where is it, shit head?")
